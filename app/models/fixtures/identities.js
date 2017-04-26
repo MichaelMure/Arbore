078f8af95e9e53ab7c8d2df5b9b6a1a36f183c2c
Michael Muré <batolettre@gmail.com>
2017-04-26 20:00:02 +0900

    some cleanup
    Signed-off-by: Michael Muré <batolettre@gmail.com>

--- app/models/fixtures/identities.js
+++ app/models/fixtures/identities.js
@@ -2,7 +2,6 @@
 import Identity from 'models/Identity'
 
 export default [
-  Identity.create("Name", "https://s3.amazonaws.com/uifaces/faces/twitter/rem/73.jpg", 'pubkey1'),
   Identity.create("Remy Sharp", "https://s3.amazonaws.com/uifaces/faces/twitter/rem/73.jpg", 'pubkey2'),
   Identity.create("Adelle Charles", "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg", 'pubkey3'),
   Identity.create("Jono Hunt", "https://s3.amazonaws.com/uifaces/faces/twitter/jonohunt/128.jpg", 'pubkey4'),
