3b8720eadea3b02542e86f44720427fff0769042
Michael Muré <batolettre@gmail.com>
2017-05-23 21:07:40 +0900

    display a helper text in the chat page when there is no contact

--- app/components/chat/ChatPage.js
+++ app/components/chat/ChatPage.js
@@ -3,10 +3,29 @@ import React, { Component } from 'react'
 import styles from './ChatPage.css'
 import RoomList from 'containers/chat/RoomList'
 import Room from 'containers/chat/Room'
+import ContactList from 'models/ContactList'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
 
 class ChatPage extends Component {
 
+  props: {
+    contacts: ContactList,
+    onGoToContactClick: () => any,
+  }
+
   render() {
+    if(this.props.contacts.contacts.count() <= 0) {
+      return (
+        <div className={styles.noContact}>
+          <Typography type="subheading">Ho no !</Typography>
+          <Typography>It seems that you have no contact yet.</Typography>
+          <div className={styles.spacer} />
+          <Button raised primary onClick={this.props.onGoToContactClick}>Go to the contact management</Button>
+        </div>
+      )
+    }
+
     return (
       <div className={styles.wrapper}>
         <div className={styles.contacts}>
