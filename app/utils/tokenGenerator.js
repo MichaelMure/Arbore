9ff684d2c0a17dc2051f5d951b2e49fac677673f
Michael Muré <batolettre@gmail.com>
2017-05-23 13:34:27 +0900

    replace half-assed crypto with a package from npm

--- app/utils/tokenGenerator.js
+++ app/utils/tokenGenerator.js
@@ -1,16 +1,8 @@
 // @flow
-import crypto from 'crypto'
+import randToken from 'rand-token'
 
-const secret = crypto.randomBytes(128).toString('hex')
-let counter = 0
+const generator = randToken.generator()
 
-// Hey random guy from the internet, does that make sense ?
 export function nextToken() {
-  counter++
-
-  return crypto.createHash('sha256')
-    .update(secret)
-    .update(counter.toString())
-    .digest('hex')
-    .slice(-32)
+  return generator.generate(32)
 }
