cd216377e4a95a544ed881612b5f7c61121e76b4
Michael Muré <batolettre@gmail.com>
2017-07-19 14:21:15 +0900

    isLocal: cascade to children only for isLocal = true, otherwise we can't tell

--- app/reducers/ipfsObject.js
+++ app/reducers/ipfsObject.js
@@ -40,7 +40,11 @@ const reducer = handleActions({
         // Set the file fully complete
         return state.set(fileWritable.sizeLocal, isLocal ? state.sizeTotal : 0)
       case ObjectType.DIRECTORY:
-        // Recursively call the reducer on children
+        // If isLocal, recursively call the reducer on children to set them all as local
+        if(!isLocal) {
+          return state
+        }
+
         return state.set(dirWritable.children,
           state.children.update((children) => children.map((obj: IpfsObject) => reducer(obj, action)))
         )
