49e78c4e59200e3f170c8b7b67c72ded4141ecfc
Michael Muré <batolettre@gmail.com>
2017-08-13 16:43:48 +0900

    InsetText: adjust colors

--- app/components/common/InsetText.js
+++ app/components/common/InsetText.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { withStyles, createStyleSheet } from 'material-ui/styles'
 import Typography from 'material-ui/Typography'
+import { fade } from 'material-ui/styles/colorManipulator'
 
 class InsetText extends Component {
 
@@ -19,24 +20,31 @@ class InsetText extends Component {
   }
 }
 
-const styleSheet = createStyleSheet('InsetText', theme => ({
-  text: {
-    minHeight: 150,
-    maxHeight: 300,
-    overflow: 'auto',
-    backgroundColor: theme.palette.background.appBar,
-    borderRadius: 5,
-    marginTop: '10px !important',
-    whiteSpace: 'pre-line',
-    padding: 2
-  },
-  placeholder: {
-    minHeight: 150,
-    color: 'gray !important',
-    backgroundColor: theme.palette.background.appBar,
-    borderRadius: 5,
-    marginTop: '10px !important',
+const styleSheet = createStyleSheet('InsetText', theme => {
+  const backgroundColor = theme.palette.grey[theme.palette.type === 'light' ? 300 : 900]
+  const color = theme.palette.getContrastText(backgroundColor)
+
+  return {
+    text: {
+      minHeight: 150,
+      maxHeight: 300,
+      overflow: 'auto',
+      backgroundColor: backgroundColor,
+      color: color,
+      borderRadius: 5,
+      marginTop: '10px !important',
+      whiteSpace: 'pre-line',
+      padding: 2
+    },
+    placeholder: {
+      minHeight: 150,
+      backgroundColor: backgroundColor,
+      color: fade(color, 0.6),
+      borderRadius: 5,
+      marginTop: '10px !important',
+      padding: 2
+    }
   }
-}))
+})
 
 export default withStyles(styleSheet)(InsetText)
