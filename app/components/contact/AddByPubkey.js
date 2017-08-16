fb26014c455565a6633dab75aa8c41a658bda19f
Michael Muré <batolettre@gmail.com>
2017-08-16 16:54:25 +0900

    AddByPubkey: fix fullWidth of the TextField

--- app/components/contact/AddByPubkey.js
+++ app/components/contact/AddByPubkey.js
@@ -19,7 +19,7 @@ class AddByPubkey extends Component {
     const { error, pristine, submitting, waiting, handleSubmit } = this.props
     return (
       <form className={styles.wrapper} onSubmit={handleSubmit}>
-        <Field name='pubkey' component={renderTextField} required label='Contact'/>
+        <Field name='pubkey' component={renderTextField} required fullWidth label='Contact'/>
 
         { error && <Error>{error}</Error>}
 
