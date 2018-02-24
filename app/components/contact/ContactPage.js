7f8583f6acbc40f6a6bb0ce2caf97fcde8309e08
Michael Muré <batolettre@gmail.com>
2018-02-24 16:11:06 +0100

    ContactPage: migrate to a css grid with the secondary menu

--- app/components/contact/ContactPage.js
+++ app/components/contact/ContactPage.js
@@ -1,9 +1,10 @@
 // @flow
 import React, { Component } from 'react'
-import styles from './ContactPage.css'
+import { withStyles } from 'material-ui/styles'
 import ContactList from 'models/ContactList'
 import Contact from 'models/Contact'
 import CompactContact from './CompactContact'
+import SecondaryMenu from 'containers/menu/SecondaryMenu'
 import ContactDetail from 'containers/contact/ContactDetail'
 import ContactAdder from 'components/contact/ContactAdder'
 import SearchField from '../SearchField'
@@ -17,36 +18,56 @@ class ContactPage extends Component {
   }
 
   render() {
-    const { contacts, onClickGenerator, onSearchChange } = this.props
+    const { classes, contacts, onClickGenerator, onSearchChange } = this.props
     const selectedPubKey = contacts.selectedPubkey
     const list = contacts.searched.sortBy((contact: Contact) => contact.identity)
 
     return (
-      <div className={styles.wrapper}>
-        <div className={styles.list} >
-          <SearchField onChange={onSearchChange} />
-          <div className={styles.scroller}>
-            <ContactAdder/>
-            {
-              list.map((contact: Contact) =>
-                <CompactContact
-                  key={ contact.pubkey }
-                  contact={contact}
-                  selected={contact.pubkey === selectedPubKey}
-                  onClick={onClickGenerator(contact.pubkey)}
-                />
-              )
-            }
-          </div>
+      <div className={classes.wrapper}>
+        <SearchField onChange={onSearchChange} />
+
+        <SecondaryMenu />
+
+        <div className={classes.list} >
+          <ContactAdder/>
+          {
+            list.map((contact: Contact) =>
+              <CompactContact
+                key={ contact.pubkey }
+                contact={contact}
+                selected={contact.pubkey === selectedPubKey}
+                onClick={onClickGenerator(contact.pubkey)}
+              />
+            )
+          }
         </div>
-        <div className={styles.details}>
-          <div className={styles.scroller}>
-            { selectedPubKey && <ContactDetail /> }
-          </div>
+
+        <div className={classes.details}>
+          { selectedPubKey && <ContactDetail /> }
         </div>
       </div>
     )
   }
 }
 
-export default ContactPage
+
+const style = theme => ({
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: '2fr 3fr',
+    gridTemplateRows: 'auto 1fr',
+    height: '100vh',
+    gridGap: '20px',
+    padding: '5px 10px 10px',
+  },
+  list: {
+    overflow: 'auto',
+  },
+  scroller: {
+    height: '100%',
+    width: '100%',
+    overflow: 'auto',
+  },
+})
+
+export default withStyles(style)(ContactPage)
