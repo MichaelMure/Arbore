7673645de04e1569de202e8dda1e7d012f4d184e
Michael Muré <batolettre@gmail.com>
2018-03-19 23:15:04 +0100

    harmonise ipfsMonoProcess with main/renderer version

--- app/ipfs/ipfsMonoProcess.js
+++ app/ipfs/ipfsMonoProcess.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { IpfsConnector, ipfsEvents } from '@michaelmure/ipfs-connector'
+import { IpfsConnector, ipfsEvents, ConnectorState } from '@michaelmure/ipfs-connector'
 
 let serviceStarted = false
 
@@ -24,54 +24,78 @@ export function waitForIpfsReady() {
 export const start = () => {
   const instance = IpfsConnector.getInstance()
 
+  const userData = app.getPath('userData')
+
+  console.log('Application data path: ' + userData)
+
+  // set the download path for ipfs
+  instance.setBinPath(userData)
+
+  // set the ipfs repository path
+  instance.setIpfsFolder(path.join(userData, '/repo'))
+
   instance.setOption('args', [
     'daemon',
-    '--enable-pubsub-experiment'
+    '--enable-pubsub-experiment',
+    '--enable-namesys-pubsub'
   ])
 
+  instance.enableDownloadEvents()
+
   // install some event listeners
-  instance.on(ipfsEvents.SERVICE_STARTED, onServiceStarted)
   instance.on(ipfsEvents.SERVICE_STARTING, onServiceStarting)
+  instance.on(ipfsEvents.SERVICE_STARTED, onServiceStarted)
+  instance.on(ipfsEvents.UPGRADING_BINARY, onServiceUpgrade)
   instance.on(ipfsEvents.SERVICE_STOPPING, onServiceStopping)
   instance.on(ipfsEvents.SERVICE_FAILED, onServiceFailed)
+  instance.on(ipfsEvents.STATUS_UPDATE, onStatusUpdate)
+  instance.on(ipfsEvents.DOWNLOAD_PROGRESS, onDownloadProgress)
+  instance.on(ipfsEvents.DOWNLOAD_ERROR, onDownloadError)
 
   // start ipfs daemon and download binaries if needed
   instance.start()
 }
 
 const onServiceStarting = async () => {
-  console.log('Main: Ipfs service starting')
+  console.log('Ipfs service starting')
 
   serviceStarted = false
 }
 
 const onServiceStarted = async () => {
-  const instance = IpfsConnector.getInstance()
-  const versionOK = await instance.checkVersion()
-
-  if(!versionOK) {
-    console.log("Need to update IPFS")
-    await instance.stop()
-    await instance.downloadManager.deleteBin()
-    instance.start()
-    return
-  }
-
   console.log('Ipfs service started')
 
   serviceStarted = true
 }
 
+const onServiceUpgrade = () => {
+  console.log('Ipfs service upgrading')
+
+  serviceStarted = false
+}
+
 const onServiceStopping = () => {
-  console.log('Main: Ipfs service stopping')
+  console.log('Ipfs service stopping')
 
   serviceStarted = false
 }
 
 const onServiceFailed = () => {
-  console.log('Main: Ipfs service failed')
+  console.log('Ipfs service failed')
 
   serviceStarted = false
+}
+
+const onStatusUpdate = (state) => {
+  console.log('Ipfs status update: ' + ConnectorState[state])
+}
+
+const onDownloadProgress = (progress) => {
+  console.log(`Ipfs daemon download progress: ${progress.completed} / ${progress.total}`)
+}
+
+const onDownloadError = (error) => {
+  console.log(`Ipfs download error: ${error}`)
 }
 
 export const stop = () => {
